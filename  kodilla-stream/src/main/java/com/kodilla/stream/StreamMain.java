4f92952454e7b7cf8bda50ebc73f94e032f346e9
KatarzynaCalka <calka.katarzyna@wp.pl>
2018-01-25 16:59:17 +0100

    Modul 07.2

---  kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++  kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.*;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
@@ -29,6 +30,9 @@ public class StreamMain {
         poemBeautifier.beautify("Kodilla Course", (textForDecoration) -> textForDecoration.toUpperCase() );
         poemBeautifier.beautify("Kodilla Course", (textForDecoration) -> (textForDecoration.replace('o','*')));
         poemBeautifier.beautify("Kodilla Course", (textForDecoration) -> (textForDecoration.replaceAll("Course", "Bootcamp")));
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
 
     }
 }
