4ae2503761fcae5c7a351d41d0f88f17eda521c7
KatarzynaCalka <calka.katarzyna@wp.pl>
2018-03-18 08:58:56 +0100

    Modul 11.5

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/builder/bigmac/BigmacTestSuite.java
@@ -0,0 +1,27 @@
+package com.kodilla.patterns.builder.bigmac;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BigmacTestSuite {
+
+    @Test
+    public void testBigmacNew() {
+        //Given
+        Bigmac bigmac = new Bigmac.BigmacBuilder()
+                .roll("Sezame")
+                .burgers(1)
+                .sauce("Barbecue")
+                .ingredient("Becon")
+                .ingredient("Cheese")
+                .ingredient("Onion")
+                .ingredient("Salad")
+                .build();
+        System.out.println(bigmac);
+        //When
+        int howManyIngredients = bigmac.getIngredients().size();
+        //Then
+        Assert.assertEquals(4, howManyIngredients);
+    }
+}
+
